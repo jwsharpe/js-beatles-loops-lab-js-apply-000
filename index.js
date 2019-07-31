8f313ebe44bb8f1a4e7bb2417fbdfc55826a322c
James Sharpe <jameswrightsharpe@gmail.com>
2019-07-31 21:11:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(members, instruments){
+  var beatleLoop = [];
+  for(var i = 0; i < 4; i++){
+    beatleLoop.push(members[i] + " plays " + instruments[i])
+  }
+  return beatleLoop;
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  var factsList = [];
+  while(i<facts.length){
+    factsList.push(facts[i]+"!!!")
+    i++;
+  }
+  return factsList
+  
+}
+
+function iLoveTheBeatles(size){
+  var message = [];
+  if(size >= 15){return message.push("I love the Beatles!")}
+  for(var i = size; i < 15; i++){
+    message.push("I love the Beatles!")
+  }
+  return message;
+}
